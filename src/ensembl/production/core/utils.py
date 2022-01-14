b0580c95fe34c071d03935dc0a6fc8b39f890b0c
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2022-01-14 18:30:24 +0000

    Following issues with some RR submission and job not retrieved from metadata - enforce removal of hidden char from perl string prior to load - added tests - Removed duplicated code and assigned retrocompat aliases

--- src/ensembl/production/core/utils.py
+++ src/ensembl/production/core/utils.py
@@ -10,12 +10,25 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from email.mime.text import MIMEText
-from smtplib import SMTP
-import json
+
 import logging
 import os
+from email.mime.text import MIMEText
+from smtplib import SMTP
+
 import pwd
+
+# retro compatibility, force import methods from actual perl_utils module
+from ensembl.production.core.perl_utils import \
+    dict_to_perl_string as pdict_to_perl_string, \
+    perl_string_to_python as pperl_string_to_python, \
+    escape_perl_string as pescape_perl_string, \
+    list_to_perl_string as plist_to_perl_string
+
+perl_string_to_python = pperl_string_to_python
+dict_to_perl_string = pdict_to_perl_string
+list_to_perl_string = plist_to_perl_string
+escape_perl_string = pescape_perl_string
 
 
 def get_default_user():
@@ -41,68 +54,15 @@ def send_email(**kwargs):
     msg['To'] = kwargs['to_address']
     smtp_server = kwargs.get('smtp_server', 'localhost')
     to_address = kwargs['to_address']
-    logger.debug('sendmail server: {} - Message from: {}, to: {}, subject: {}'.format(smtp_server, from_address, to_address, msg['Subject']))
+    logger.debug(
+        'sendmail server: {} - Message from: {}, to: {}, subject: {}'.format(smtp_server, from_address, to_address,
+                                                                             msg['Subject']))
     s = SMTP(smtp_server)
     s.sendmail(from_address, (to_address,), msg.as_string())
     s.quit()
 
 
-def dict_to_perl_string(input_dict):
-    """Transform the supplied dict into a string representation of a Perl hash"""
-    pairs = []
-    for k,v in sorted(filter(lambda k_v: k_v[1] != None, input_dict.items())):
-        k = str(k)
-        t = type(v).__name__
-        if t == 'str':
-            pairs.append("\"%s\" => \"%s\"" % (k,escape_perl_string(v)))
-        elif (t == 'int') :
-            pairs.append("\"%s\" => %d" % (k,v))
-        elif t == 'float':
-            pairs.append("\"%s\" => %f" % (k,v))
-        elif t == 'list':
-            pairs.append("\"%s\" => %s" % (k,list_to_perl_string(v)))
-        elif t == 'dict':
-            pairs.append("\"%s\" => %s" % (k,dict_to_perl_string(v)))
-        elif t == 'bool':
-            if str(v) == "True":
-                pairs.append("\"%s\" => %d" % (k,1))
-        else:
-            raise Exception("Unsupported type "+str(t))
-    return "{%s}" % ", ".join(pairs)
-
-def list_to_perl_string(input_list):
-    """Transform the supplied array into a string representation of a Perl array"""
-    elems = []
-    for v in input_list:
-        t = type(v).__name__
-        if t == 'str':
-            elems.append("\"%s\"" % escape_perl_string(v))
-        elif(t == 'int'):
-            elems.append("%d" % v)
-        elif t == 'float':
-            elems.append("%f" % v)
-        elif t == 'list':
-            elems.append("%s" % list_to_perl_string(v))
-        elif t == 'dict':
-            elems.append("%s" % dict_to_perl_string(v))
-        else:
-            raise Exception("Unsupported type "+str(t))
-    return "[%s]" % ", ".join(elems)
-
-def escape_perl_string(v):
-    """Escape characters with special meaning in perl"""
-    return str(v).replace("$","\\$").replace("\"","\\\"").replace("@","\\@")
-
 def json_decode_error_context(error):
     beg = max(0, error.pos - 25)
     end = min(len(error.doc), error.pos + 25)
     return f"{error}. --> {error.doc[beg:end]} <--"
-
-def perl_string_to_python(s):
-    """Parse a Perl hash string into a Python dict"""
-    s = s.replace("=>",":").replace("\\$","$").replace("\\@","@")
-    try:
-        res = json.loads(s)
-    except json.JSONDecodeError as err:
-        raise ValueError(f"Invalid JSON: {json_decode_error_context(err)}")
-    return res
